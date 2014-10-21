1a49fa17f273c294e9617f9d4bd5db4216657ffc
Ed Lewis <edward.richard.lewis@gmail.com>
2014-10-21 20:31:48 +0100

    started rooms code + rooms test for get room

--- src/main/java/hipchat4j/Rooms.java
+++ src/main/java/hipchat4j/Rooms.java
@@ -1,8 +1,30 @@
 package hipchat4j;
 
+import hipchat4j.connector.ConnectorAbstract;
+import hipchat4j.entities.Room;
+import hipchat4j.json.JsonParser;
+
 /**
  * hipchat4j
  * 09/10/14/20:09
  */
 public class Rooms {
+
+    private final ConnectorAbstract connector;
+
+    public Rooms(ConnectorAbstract c) {
+        connector = c;
+    }
+
+    public Room getRoom(int id)
+    {
+        return getRoom(""+id);
+    }
+
+    public Room getRoom(String name) {
+        String resp = connector.get("/v2/room/"+name);
+        return JsonParser.getInstance().fromJson(resp, Room.class);
+    }
+
+
 }
---
+++ src/test/java/hipchat4j/RoomsTest.java
@@ -0,0 +1,40 @@
+package hipchat4j;
+
+import hipchat4j.config.Config;
+import hipchat4j.connector.ConnectorMock;
+import hipchat4j.entities.Room;
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RoomsTest {
+
+    private Rooms rooms;
+    private ConnectorMock cm;
+
+    @Before
+    public void setUp() throws Exception {
+        cm = new ConnectorMock(new Config("hello", "world"));
+        rooms = new Rooms(cm);
+        cm.addResponseMapping("/v2/room/abc", "200", IOUtils.toString(this.getClass().getResourceAsStream("/room_full.json")));
+        cm.addResponseMapping("/v2/room/123", "200", IOUtils.toString(this.getClass().getResourceAsStream("/room_full.json")));
+    }
+
+    @Test
+    public void testGetRoom() throws Exception {
+        Room r = rooms.getRoom("abc");
+        assertNotNull(r);
+        assertEquals("/v2/room/abc", cm.getLastGetRequest());
+        assertEquals("roomname", r.getName());
+    }
+
+    @Test
+    public void testGetRoomInt() throws Exception {
+        Room r = rooms.getRoom(123);
+        assertNotNull(r);
+        assertEquals("/v2/room/123", cm.getLastGetRequest());
+        assertEquals("roomname", r.getName());
+    }
+}
