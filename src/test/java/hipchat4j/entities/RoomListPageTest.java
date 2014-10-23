2a764a4865025dd06dca5986de40b2fa32101bed
Ed Lewis <edward.richard.lewis@gmail.com>
2014-10-23 19:32:57 +0100

    added json inflation test

--- src/test/java/hipchat4j/entities/RoomListPageTest.java
+++ src/test/java/hipchat4j/entities/RoomListPageTest.java
@@ -1,5 +1,7 @@
 package hipchat4j.entities;
 
+import hipchat4j.json.JsonParser;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -53,5 +55,30 @@ public class RoomListPageTest {
     @Test
     public void testGetMaxResults() throws Exception {
         assertEquals(100, rlp.getMaxResults());
+    }
+
+    @Test
+    public void testExpectedJSON() throws Exception {
+
+        String json = IOUtils.toString(this.getClass().getResourceAsStream("/getrooms_expected_output.json"));
+
+        RoomListPage r = JsonParser.getInstance().fromJson(json, RoomListPage.class);
+
+        assertNotNull(r);
+        List<Item> items = r.getItems();
+        assertNotNull(items);
+        assertEquals(1, items.size());
+        assertEquals(99, items.get(0).getId());
+        assertEquals("roomname", items.get(0).getName());
+        assertEquals("self", items.get(0).getLinks().getSelf());
+        assertEquals("webhooks", items.get(0).getLinks().getWebhooks());
+        assertEquals("members", items.get(0).getLinks().getMembers());
+        assertEquals("participants", items.get(0).getLinks().getParticipants());
+        assertEquals("pageself", r.getLinks().getSelf());
+        assertEquals("pageprev", r.getLinks().getPrev());
+        assertEquals("pagenext", r.getLinks().getNext());
+        assertEquals(99, r.getMaxResults());
+        assertEquals(0, r.getStartIndex());
+
     }
 }
