13d067530bd3f2728113644c6e8f8cf8a4e7fa74
Ed Lewis <edward.richard.lewis@gmail.com>
2014-10-26 15:35:02 +0000

    added json response test

--- src/test/java/hipchat4j/entities/RoomCreateResponseTest.java
+++ src/test/java/hipchat4j/entities/RoomCreateResponseTest.java
@@ -1,5 +1,7 @@
 package hipchat4j.entities;
 
+import hipchat4j.json.JsonParser;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,5 +24,13 @@ public class RoomCreateResponseTest {
     @Test
     public void testGetId() throws Exception {
         assertEquals("134", rcr.getId());
+    }
+
+    @Test
+    public void testFromJSON() throws Exception {
+        RoomCreateResponse rc = JsonParser.getInstance().fromJson(IOUtils.toString(this.getClass().getResourceAsStream("/create_room_response.json")), RoomCreateResponse.class);
+        assertNotNull(rc);
+        assertEquals("self",rc.getLinks().getSelf());
+        assertEquals("123", rc.getId());
     }
 }
