f5902de644f271f76a7338b0fc87ac8305d4aeb6
Ed Lewis <edward.richard.lewis@gmail.com>
2014-11-01 14:51:51 +0000

    continued with message entity + started tests

---
+++ src/test/java/hipchat4j/entities/MessageTest.java
@@ -0,0 +1,15 @@
+package hipchat4j.entities;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MessageTest {
+
+    private Message messageStyle1, messageStyle2;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
